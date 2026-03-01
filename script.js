6029a1c1b265d87ca63007e06cda5870f1e451ab
azadanielelrom4no-creator <aza.danielelrom4no@gmail.com>
2026-03-01 12:10:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function signup() {
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
 
+localStorage.setItem("user", user);
+localStorage.setItem("pass", pass);
+
+alert("Account created!");
+window.location.href = "login.html";
+}
+
+function login() {
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+if(user === localStorage.getItem("user") &&
+pass === localStorage.getItem("pass")) {
+
+localStorage.setItem("loggedIn", "true");
+window.location.href = "dashboard.html";
+
+} else {
+alert("Wrong login");
+}
+}
+
+function logout() {
+localStorage.removeItem("loggedIn");
+window.location.href = "index.html";
+}
